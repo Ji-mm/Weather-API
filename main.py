3052f44ff2f62d945d38916cda784d4959830634
Atudo <jimpirate49@gmail.com>
2023-02-16 17:19:50 +0300

    refactoring

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ import pandas as pd
 
 app = Flask(__name__)
 
+# Access data from file
 stations = pd.read_csv("data_small/stations.txt", skiprows=17)
 stations = stations[["STAID", "STANAME                                 "]]
 
 
+# Create a home page
 @app.route("/")
 def home():
     return render_template("home.html", data=stations.to_html())
 
 
+# Create end point using station and date
 @app.route("//api/v1/<station>/<date>/")
 def about(station, date):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
@@ -22,6 +25,7 @@ def about(station, date):
             "temperature": temperature}
 
 
+# Create end point using station
 @app.route("/api/v1/<station>/")
 def all_data(station):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
@@ -30,6 +34,7 @@ def all_data(station):
     return results
 
 
+# Create end point using station and year
 @app.route("/api/v1/yearly/<station>/<year>")
 def yearly(station, year):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
